f3678dfdbddc6f1e208a77d14c63698b9df10778
techcloud.spector <techcloud.spector@gmail.com>
2026-01-18 22:12:37 +0000

    chunk

--- App.tsx
+++ App.tsx
@@ -1,37 +1,40 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import PcdLayout from './layouts/PcdLayout';
-import OverviewPage from './pages/pcd/OverviewPage';
-import ArchitecturePage from './pages/pcd/ArchitecturePage';
-import WorkflowsPage from './pages/pcd/WorkflowsPage';
-import ResourcesPage from './pages/pcd/ResourcesPage';
-import LearnDetailsPage from './pages/pcd/LearnDetailsPage';
-import TechArchitecturePage from './pages/pcd/TechArchitecturePage';
-import ResearchPage from './pages/pcd/ResearchPage';
+
+const LandingPage = lazy(() => import('./pages/LandingPage'));
+const PcdLayout = lazy(() => import('./layouts/PcdLayout'));
+const OverviewPage = lazy(() => import('./pages/pcd/OverviewPage'));
+const ArchitecturePage = lazy(() => import('./pages/pcd/ArchitecturePage'));
+const WorkflowsPage = lazy(() => import('./pages/pcd/WorkflowsPage'));
+const ResourcesPage = lazy(() => import('./pages/pcd/ResourcesPage'));
+const LearnDetailsPage = lazy(() => import('./pages/pcd/LearnDetailsPage'));
+const TechArchitecturePage = lazy(() => import('./pages/pcd/TechArchitecturePage'));
+const ResearchPage = lazy(() => import('./pages/pcd/ResearchPage'));
 
 const App: React.FC = () => {
   return (
     <HashRouter>
-      <Routes>
-        {/* Public Landing Page */}
-        <Route path="/" element={<LandingPage />} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {/* Public Landing Page */}
+          <Route path="/" element={<LandingPage />} />
 
-        {/* Research Content Routes (Hidden under /pcd prefix) */}
-        <Route path="/pcd" element={<PcdLayout />}>
-          <Route index element={<Navigate to="/pcd/overview" replace />} />
-          <Route path="overview" element={<OverviewPage />} />
-          <Route path="architecture" element={<ArchitecturePage />} />
-          <Route path="learn-details" element={<LearnDetailsPage />} />
-          <Route path="tech-architecture" element={<TechArchitecturePage />} />
-          <Route path="workflows" element={<WorkflowsPage />} />
-          <Route path="resources" element={<ResourcesPage />} />
-          <Route path="research" element={<ResearchPage />} />
-        </Route>
+          {/* Research Content Routes (Hidden under /pcd prefix) */}
+          <Route path="/pcd" element={<PcdLayout />}>
+            <Route index element={<Navigate to="/pcd/overview" replace />} />
+            <Route path="overview" element={<OverviewPage />} />
+            <Route path="architecture" element={<ArchitecturePage />} />
+            <Route path="learn-details" element={<LearnDetailsPage />} />
+            <Route path="tech-architecture" element={<TechArchitecturePage />} />
+            <Route path="workflows" element={<WorkflowsPage />} />
+            <Route path="resources" element={<ResourcesPage />} />
+            <Route path="research" element={<ResearchPage />} />
+          </Route>
 
-        {/* Fallback */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+          {/* Fallback */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Suspense>
     </HashRouter>
   );
 };
---
+++ dist/assets/layers-DeGCi6KV.js
@@ -0,0 +1,16 @@
+import{c as a}from"./createLucideIcon-B2av_q7f.js";/**
+ * @license lucide-react v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M12 5v14",key:"s699le"}],["path",{d:"m19 12-7 7-7-7",key:"1idqje"}]],r=a("arrow-down",o);/**
+ * @license lucide-react v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]],d=a("arrow-right",e);/**
+ * @license lucide-react v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M12.83 2.18a2 2 0 0 0-1.66 0L2.6 6.08a1 1 0 0 0 0 1.83l8.58 3.91a2 2 0 0 0 1.66 0l8.58-3.9a1 1 0 0 0 0-1.83z",key:"zw3jo"}],["path",{d:"M2 12a1 1 0 0 0 .58.91l8.6 3.91a2 2 0 0 0 1.65 0l8.58-3.9A1 1 0 0 0 22 12",key:"1wduqc"}],["path",{d:"M2 17a1 1 0 0 0 .58.91l8.6 3.91a2 2 0 0 0 1.65 0l8.58-3.9A1 1 0 0 0 22 17",key:"kqbvx6"}]],s=a("layers",t);export{r as A,s as L,d as a};
